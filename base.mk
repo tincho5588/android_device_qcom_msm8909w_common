d9d57152bc8594c97d10cec9dc4123f18ac8dfad
Mohammed Thasleem <mthasl@codeaurora.org>
2016-02-24 20:39:21 +0530

    8909w: fix ringtone issue caused disabling  video codecs
    fix ringtone issue caused disableing  video codecs
    functionality for 8909 wearable devices.
    
    Change-Id: I8c9d402c94621145a15a3afb9072bf2ec598ccf8

--- base.mk
+++ base.mk
@@ -817,13 +817,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     device/qcom/common/media/media_profiles.xml:system/etc/media_profiles.xml \
     device/qcom/common/media/media_codecs.xml:system/etc/media_codecs.xml
-
-ifneq ($(TARGET_SUPPORTS_WEARABLES),true)
-PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
-endif
 
 # enable overlays to use our version of
 # source/resources etc.
